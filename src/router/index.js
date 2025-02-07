d90fbc4d6a9930cc8fe7f7d55cb60d4b1494c52f
Mladen Bolic <mladen.bolic@sixhours.io>
2025-02-07 12:17:42 +0100

    Vue SSR set up Vue Router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import {
+  createRouter as createVueRouter,
+  createMemoryHistory,
+  createWebHistory,
+} from "vue-router";
+import HomePage from "@/pages/HomePage.vue";
+import AboutPage from "@/pages/AboutPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutPage,
+  },
+];
+
+const isServer = typeof window === "undefined";
+
+const createRouter = () =>
+  createVueRouter({
+    history: isServer
+      ? createMemoryHistory()
+      : createWebHistory(process.env.BASE_URL),
+    routes,
+  });
+
+export default createRouter;
